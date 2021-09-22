866cef60c5283e5a2fdc2580f34d67c1b01e6065
AleksandrNebesnyi <82997944+AleksandrNebesnyi@users.noreply.github.com>
2021-09-22 16:22:06 +0300

    add throtle and accordion

---
+++ js/Create debounce-and-throttle.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/**
+ * Create debounce and throttle
+ * Write in console scroll time
+ */
+
+const div = document.querySelector('#my-id');
+
+const handleScroll = () => {
+  console.log('scroll', new Date());
+};
+
+// div.addEventListener('scroll', _.throttle(handleScroll, 2000));
+// div.addEventListener('scroll', _.debounce(handleScroll, 2000));
+function throttle(callback, delay) {
+  let id;
+
+  return function () {
+    if (id) {
+      return;
+    }
+
+    id = setTimeout(() => {
+      callback();
+      id = 0;
+    }, delay);
+  };
+}
+
+div.addEventListener('scroll', throttle(handleScroll, 2000));
+
+function debounce(callback, delay) {
+  let id;
+
+  return function () {
+    if (id) {
+      clearTimeout(id);
+    }
+
+    id = setTimeout(() => {
+      callback();
+      id = 0;
+    }, delay);
+  };
+}
+
+// div.addEventListener('scroll', debounce(handleScroll, 2000));
